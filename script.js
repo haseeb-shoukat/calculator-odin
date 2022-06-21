90891b61aec7959e0728d957de417af04f2a0274
hasemon <abdulhaseebshoukat24@outlook.con>
2022-06-21 11:37:29 +0500

    Create operate function that calls one of the basic functions

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 function add(n1, n2) {
-    n1 + n2
+    return (n1 + n2)
 }
 
 function subtract(n1, n2) {
-    n1 - n2
+    return (n1 - n2)
 }
 
 function multiply(n1, n2) {
-    n1 * n2
+    return (n1 * n2)
 }
 
 function add(n1, n2) {
-    n1 / n2
+    return (n1 / n2)
+}
+
+operate (op, n1, n2) {
+    switch (op) {
+        case "+": 
+            return add(n1, n2);
+        case "-":
+            return subtract(n1, n2);
+        case "*":
+            return multiply(n1, n2);
+        case "/":
+            return divide(n1, n2)
+    }
 }
